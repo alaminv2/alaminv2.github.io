22c30c14fe0148c7037b34b77b83117594476da6
alaminv2 <alamin.bsmrstu.cse@gmail.com>
2020-11-02 19:55:59 +0600

    add guessing game

---
+++ GuessingGame/script.js
@@ -0,0 +1,31 @@
+let check = true;
+let body = document.querySelector('#main');
+let ans = Math.floor(Math.random() * 140 + 10);
+
+for( i = 1; i <= 3; i++){
+    console.log(ans);
+    let num = parseInt(prompt('Guess a number between 10 to 50'));
+    if (ans === num){
+        // alert('Congratulations You win.....');
+        let h3 = document.createElement('h3');
+        h3.classList.add('right');
+        h3.innerHTML = 'Congratulations You Win...!';
+        body.appendChild(h3);
+        check = true;
+        break;
+    }
+    else if(num > ans){
+        alert('Correct answer is smaller!');
+        check = false;
+    }
+    else{
+        alert('Correct answer is greater!');
+        check = false;
+    }
+}
+if(check === false){
+    let h3 = document.createElement('h3');
+    h3.classList.add('wrong');
+    h3.innerHTML = 'You Lose...!';
+    body.appendChild(h3);
+}
