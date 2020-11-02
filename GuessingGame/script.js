fc47ba922e7cd44dc11407434b834fce0b4fe338
alaminv2 <alamin.bsmrstu.cse@gmail.com>
2020-11-02 20:15:23 +0600

    number overload controlled

--- GuessingGame/script.js
+++ GuessingGame/script.js
@@ -4,22 +4,28 @@ let ans = Math.floor(Math.random() * 140 + 10);
 
 for( i = 1; i <= 3; i++){
     console.log(ans);
-    let num = parseInt(prompt('Guess a number between 10 to 150'));
-    if (ans === num){
-        // alert('Congratulations You win.....');
-        let h3 = document.createElement('h3');
-        h3.classList.add('right');
-        h3.innerHTML = 'Congratulations You Win...!';
-        body.appendChild(h3);
-        check = true;
-        break;
-    }
-    else if(num > ans){
-        alert('Correct answer is smaller!');
-        check = false;
+    let num = parseInt(prompt('Guess a number between 10 and 150'));
+    if(num <= 150 && num >=10){
+        if (ans === num){
+            // alert('Congratulations You win.....');
+            let h3 = document.createElement('h3');
+            h3.classList.add('right');
+            h3.innerHTML = 'Congratulations You Win...!';
+            body.appendChild(h3);
+            check = true;
+            break;
+        }
+        else if(num > ans){
+            alert('Correct answer is smaller!');
+            check = false;
+        }
+        else{
+            alert('Correct answer is greater!');
+            check = false;
+        }
     }
     else{
-        alert('Correct answer is greater!');
+        alert('Please enter a number between 10 and 150.')
         check = false;
     }
 }
